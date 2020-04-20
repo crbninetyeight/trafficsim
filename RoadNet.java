784686b2bbf5c0b9fea94f089f2b85a34df84e6c
Christian <christianbyrne2012@gmail.com>
2020-04-20 08:09:06 -0400

    initial staging

---
+++ RoadNet.java
@@ -0,0 +1,8 @@
+
+public class RoadNet {
+    private RoadIntersection intersectionA, intersectionB;
+
+    public RoadNet() {
+        intersectionA.join(intersectionB);
+    }
+}
---
+++ Simulation.java
@@ -0,0 +1,7 @@
+
+public class Simulation {
+    private RoadNet roadNetwork;
+    public static void main(String args[]) {
+
+    }
+}
